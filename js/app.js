2c6137a268cc8b4ac66bded74fc59b3331ed72d0
Alireza Soheili <71215827+alireza-constantin@users.noreply.github.com>
2021-08-05 12:40:45 +0430

    add footer and logIn modal

--- js/app.js
+++ js/app.js
@@ -12,19 +12,16 @@ const sportBtn = document.getElementById('sports-btn');
 const newsBtn = document.getElementById('news-btn');
 const eventBtn = document.getElementById('events-btn');
 
-// newsBtn.addEventListener('click', () => {
-// 	sportLive.style.display = 'none';
-// 	breakingNews.style.display = 'block';
-// });
-
-function changeBackground(el) {}
+const modal = document.querySelector('.modal');
+const loginBtn = document.querySelector('.login-btn');
+const closeBtn = document.querySelector('.close');
 
 function hideElement(el) {
 	el.style.display = 'none';
 }
 
 function hideBorder(el) {
-	el.style.borderBottom = 'none';
+	el.classList.remove('selected');
 }
 
 function hideAll(el, borderEl) {
@@ -37,7 +34,7 @@ function showElement(el) {
 }
 
 function showBorder(el) {
-	el.style.borderBottom = '6px solid #fff';
+	el.classList.add('selected');
 }
 
 function showAll(el, borderEl) {
@@ -61,11 +58,13 @@ function displayEL(showEl) {
 	}
 	setTimeout(() => {
 		container.style.animation = '';
+		liveSports.style.animation = '';
 	}, 10);
 }
 
 liveNav.addEventListener('click', (event) => {
 	container.style.animation = 'none';
+	liveSports.style.animation = 'none';
 	if (event.target.id === 'news-btn') {
 		displayEL(breakingNews);
 		if (mediaQuery.matches) {
@@ -93,5 +92,19 @@ liveNav.addEventListener('click', (event) => {
 			liveSports.style.background =
 				"url('/img/live-sports.jpg') no-repeat center center / cover";
 		}
+	}
+});
+
+closeBtn.addEventListener('click', () => {
+	modal.style.display = 'none';
+});
+
+loginBtn.addEventListener('click', () => {
+	modal.style.display = 'block';
+});
+
+window.addEventListener('click', (e) => {
+	if (e.target == modal) {
+		modal.style.display = 'none';
 	}
 });
